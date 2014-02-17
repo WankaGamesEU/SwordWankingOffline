1e4b6593107e017ebe0044ca00a6b6d041ede20e
wwwking123 <wwwmrlol@gmail.com>
2014-02-17 22:34:24 +0100

    Game

---
+++ HugeWorldRPG/Assets/PlayerScripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float playerMovementSpeed = 5.0f;
+	public float mouseSens = 5.0f;
+	public float rangeUpDown = 70f;
+	float verticalRotation = 0;
+	public float verticalVelocity = 0;
+	public float jumpSpeed = 5f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		// CameraRot
+
+			//Horizontal
+		float rotLeftRight = Input.GetAxis("Mouse X") * mouseSens;
+		transform.Rotate(0 , rotLeftRight, 0);
+
+			//Vertical
+		verticalRotation -= Input.GetAxis("Mouse Y") * mouseSens;
+		verticalRotation = Mathf.Clamp (verticalRotation, -rangeUpDown, rangeUpDown);
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);
+
+
+
+
+		//MoveMentCommands
+		float ForwardSpeed = Input.GetAxis ("Vertical") * playerMovementSpeed;
+		float SideSpeed = Input.GetAxis ("Horizontal")* playerMovementSpeed;
+
+		Vector3 speed = new Vector3(SideSpeed, verticalVelocity, ForwardSpeed);
+
+		speed = transform.rotation * speed;
+	
+		CharacterController cc = GetComponent<CharacterController> ();
+		cc.Move (speed * Time.deltaTime);
+
+
+		//Gravity
+
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+
+
+
+
+		if (cc.isGrounded)  {
+
+		//Jump
+		
+		if (Input.GetButton ("Jump")) 	{
+			verticalVelocity = jumpSpeed;
+			}
+		}
+	}
+}
---
+++ HugeWorldRPG/Assets/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public int bulletDmg = 25;
+
+	// Update is called once per frame
+	void update () {
+	
+		Debug.DrawRay (transform.position, Vector3.forward);
+		RaycastHit hit;
+		Ray bullet = new Ray(transform.position, Vector3.forward);
+		if (Input.GetMouseButtonDown (0))
+		{
+
+			if(Physics.Raycast(bullet, out hit, 100))
+			{
+				{
+					hit.transform.SendMessage ("Damage", bulletDmg, SendMessageOptions.DontRequireReceiver);
+				}
+		}
+	}
+}
+}
